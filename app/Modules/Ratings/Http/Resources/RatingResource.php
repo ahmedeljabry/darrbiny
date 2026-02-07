8562b037bd7272a7a696f798def5edfc070ee0e8
Ahmed-Elgabry <ahmedeljabry19@gmail.com>
2026-02-07 19:37:59 +0200

    update rating

--- app/Modules/Ratings/Http/Resources/RatingResource.php
+++ app/Modules/Ratings/Http/Resources/RatingResource.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Modules\Ratings\Http\Resources;
 
+use App\Models\City;
+use App\Models\Country;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class RatingResource extends JsonResource
@@ -45,8 +47,18 @@ class RatingResource extends JsonResource
             return null;
         }
 
-        $country = $trainerProfile->country;
-        $city = $trainerProfile->city;
+        $countryId = $this->resolveTrainerProfileValue($trainerProfile, 'country_id');
+        $cityId = $this->resolveTrainerProfileValue($trainerProfile, 'city_id');
+
+        $country = $trainerProfile->relationLoaded('country') ? $trainerProfile->country : null;
+        if ($countryId && (!$country || $country->id !== $countryId)) {
+            $country = $this->findCountry($countryId);
+        }
+
+        $city = $trainerProfile->relationLoaded('city') ? $trainerProfile->city : null;
+        if ($cityId && (!$city || $city->id !== $cityId)) {
+            $city = $this->findCity($cityId);
+        }
 
         return [
             'country' => $country ? [
@@ -67,19 +79,23 @@ class RatingResource extends JsonResource
             return null;
         }
 
+        $carType = $this->resolveTrainerProfileValue($trainerProfile, 'car_type');
+        $carModelYear = $this->resolveTrainerProfileValue($trainerProfile, 'car_model_year');
+        $carAvailable = $this->resolveTrainerProfileValue($trainerProfile, 'car_available');
+
         $carName = null;
-        if ($trainerProfile->car_type) {
-            $carName = $trainerProfile->car_type;
-            if ($trainerProfile->car_model_year) {
-                $carName .= ' ' . $trainerProfile->car_model_year;
+        if ($carType) {
+            $carName = $carType;
+            if ($carModelYear) {
+                $carName .= ' ' . $carModelYear;
             }
         }
 
         return [
-            'type' => $trainerProfile->car_type,
-            'model_year' => $trainerProfile->car_model_year,
+            'type' => $carType,
+            'model_year' => $carModelYear,
             'name' => $carName,
-            'available' => $trainerProfile->car_available ?? false,
+            'available' => (bool) ($carAvailable ?? false),
         ];
     }
 
@@ -93,6 +109,44 @@ class RatingResource extends JsonResource
             $parts[] = $country->name;
         }
         return implode(', ', $parts) ?: '';
+    }
+
+    private function resolveTrainerProfileValue($trainerProfile, string $field)
+    {
+        if (!$trainerProfile) {
+            return null;
+        }
+
+        $value = $trainerProfile->getAttribute($field);
+        if (
+            $trainerProfile->pending_approval
+            && is_array($trainerProfile->pending_changes)
+            && array_key_exists($field, $trainerProfile->pending_changes)
+        ) {
+            $value = $trainerProfile->pending_changes[$field];
+        }
+
+        return $value;
+    }
+
+    private function findCountry(string $id): ?Country
+    {
+        static $cache = [];
+        if (array_key_exists($id, $cache)) {
+            return $cache[$id];
+        }
+        $cache[$id] = Country::find($id);
+        return $cache[$id];
+    }
+
+    private function findCity(string $id): ?City
+    {
+        static $cache = [];
+        if (array_key_exists($id, $cache)) {
+            return $cache[$id];
+        }
+        $cache[$id] = City::find($id);
+        return $cache[$id];
     }
 }
 
