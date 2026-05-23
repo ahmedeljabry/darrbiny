6550a7f993bbc7e6cae78005b479e19e88d3278a
ahmedeljabry <ahmedeljabry19@gmail.com>
2026-05-23 19:48:18 +0300

    Serve landing page with explicit html response

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->file(public_path('landing/index.html'));
+    return response(file_get_contents(public_path('landing/index.html')), 200, [
+        'Content-Type' => 'text/html; charset=UTF-8',
+    ]);
 });
 
 Route::get('/login', function () {
